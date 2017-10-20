c8fe43ad4fb44aa7e553b59891dceebb87f08f41
Coconut2000 <31667191+Coconut2000@users.noreply.github.com>
2017-10-20 10:59:52 -0700

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,37 @@
 ## functions do
 
 ## Write a short comment describing this function
+## makeCacheMatrix() checks the defined matrix 
+## Checks its dimension to make sure it is a square matrix
+## Checks the square matrix to make sure its determinant is not zero
+## If the defined matrix has inverse, store its value in makeCacheMatrix()
+## Call makeCacheMatrix() in cacheSolve() to get the inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    c <- ncol(x)
+    
+    r <- nrow(x)
+    
+    if (c != r){
+       
+       
+          print("This matrix has no inverse because it is not a square matrix.")
+    }else {
+          d <- det(x)
+          if (d==0){print("This matrix has no inverse because it is determinant is zero.")
+          }else{ myMatrix <- x
+              myMatrix
+          }
+         
+    }
+        
+ 
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  if (is.numeric(makeCacheMatrix(x))){solve(makeCacheMatrix(x))}
         ## Return a matrix that is the inverse of 'x'
 }
